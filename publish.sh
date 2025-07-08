333426c099532a92078c28553c0f4618e14403fd
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-07-08 21:24:28 +0200

    ....

--- publish.sh
+++ publish.sh
@@ -3,17 +3,13 @@
 #exit at failure
 set -e
 
-# first check if you haven't forgotten to push something to remote
-# it has to work this way because else things gets overwritten pain xd
-
 git checkout github-pages
+git rebase main
 
 bun run build-only
 
 git add .
 git commit -m "updated build"
-
-git merge main -m "merged from main branch" --no-ff 
 git push
 
 git checkout main
